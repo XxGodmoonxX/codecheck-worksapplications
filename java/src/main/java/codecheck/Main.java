f0752a04b8ea1fa189eccd7297e750823e438300
Motoki Watanabe <knd.sbz.mrs.rg.tg.uhr.uol@gmail.com>
2018-01-04 23:37:03 +0900

    いろいろやってみた

--- java/src/main/java/codecheck/Main.java
+++ java/src/main/java/codecheck/Main.java
@@ -1,7 +1,12 @@
 package codecheck;
 
+import java.time.LocalDateTime;
+
 public class Main {
 	public static void main(String... args) {
 		System.out.println("Hello, world!");
+
+		LocalDateTime d = LocalDatetime.now();
+		System.out println(d.getYear());
 	}
 }
